480083bb02c995e18674539ef0b887a5914ed4ea
Daniel O'Connor <danielajoconnor@gmail.com>
2023-02-09 11:35:07 +0000

    added sql to books by id

--- pages/api/book-by-id.js
+++ pages/api/book-by-id.js
@@ -1,30 +1,18 @@
-export default function handler(req, res) {
+import sql from "@/utils/postgres";
+
+export default async function handler(req, res) {
   const { id } = req.query;
-  const books = [
-    {
-      id: "1",
-      title: "The Hobbit",
-      author: "J.R.R. Tolkien",
-    },
-    {
-      id: "2",
-      title: "The Fellowship of the Ring",
-      author: "J.R.R. Tolkien",
-    },
-    {
-      id: "3",
-      title: "Born a Crime",
-      author: "Trevor Noah",
-    },
-  ];
+  const books = await sql`
+    SELECT * FROM books
+    WHERE id = ${id}
+  `;
 
-  const foundBook = books.find((book) => {
-    if (book.id === id) {
-      return true;
-    } else {
-      return false;
-    }
-  });
+  if (books.length < 1) {
+    res.status(404).json({ message: "Book not found" });
+    return;
+  }
 
-  res.json({books: foundBook});
+  const foundBook = books[0];
+
+  res.json({ books: foundBook });
 }
